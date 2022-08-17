2adb5ec90524f9bceb1078261d328f201f760a05
RieserStern <rieserstern@gmail.com>
2022-08-17 17:53:45 -0400

    added cookie consent

--- src/App.js
+++ src/App.js
@@ -19,13 +19,24 @@ import Home from "./Pages/Home";
 import ThemeToggler, { useDarkMode } from "./Components/ThemeToggler";
 import { darkTheme, lightTheme } from "./Constants/Theme";
 
+import CookieConsent, { getCookieConsentValue } from "react-cookie-consent";
+
 export const AppUrl = "http://localhost:3001";
-export const AppUrlNoPort = "http://localhost";
-export const SocketUrl = "http://localhost:3002";
+// export const AppUrlNoPort = "http://localhost";
+// export const SocketUrl = "http://localhost:3002";
 
 
 const App = () => {
   const dispatch = useDispatch();
+
+  const cookieConsent = getCookieConsentValue();
+  const [checkedOne, setCheckedOne] = useState(false);
+  const [checkedTwo, setCheckedTwo] = useState(false);
+
+  const [necessaryConsent, updateNecessaryConsent] = useState("denied");
+  const [analyticsConsent, updateAnalyticsConsent] = useState("denied");
+  const [marketingConsent, updateMarketingConsent] = useState("denied");
+
 
   const [theme, toggleTheme] = useDarkMode();
   const themeMode = theme === "light" ? lightTheme : darkTheme;
@@ -33,13 +44,13 @@ const App = () => {
   const [loginStatus, setLoginStatus] = useState("");
 
   const getUserLoginStatus = () => {
-    Axios.get(`${AppUrl}/login`).then((response) => {
-      if (response.data.loggedIn === true) {
-        setLoginStatus(true);
-      } else {
-        // Navigate('/');
-      }
-    });
+    // Axios.get(`${AppUrl}/login`).then((response) => {
+    //   if (response.data.loggedIn === true) {
+    //     setLoginStatus(true);
+    //   } else {
+    //     // Navigate('/');
+    //   }
+    // });
   };
 
   useEffect(() => {
@@ -58,6 +69,27 @@ const App = () => {
             currency={currency}
           />
           <Navbar loginStatus={loginStatus} />
+          {/* saves the user's response */}
+          <CookieConsent
+              onAccept={() => {
+                updateNecessaryConsent("granted");
+                if (checkedOne === true) {
+                  console.log('YAY')
+                  updateAnalyticsConsent("granted");
+                }
+                if (checkedTwo === true) {
+                  updateMarketingConsent("granted");
+                }
+              }}
+              buttonText="Accept Cookies"
+              ariaAcceptLabel="Accept cookies"
+              ariaDeclineLabel="Decline cookies"
+              expires={150}
+            >
+              We use cookies and other tracking technologies to operate the website, measure and analyze the audience, present you with personalized advertising outside the site, and enable certain social networks to function.{" "}
+              <br/><br/>
+              For more detailed information about personal data collected via cookies and the cookies we use, click here.
+          </CookieConsent>
           <Routes>
             {/* Unprotected Routes */}
             <Route path="/" element={<Home theme={theme} />} />
